a45c10a33200cb28f82e245c3c726fd0aff5069d
Ruben Kluft <ruben@123inkt.nl>
2025-10-08 15:51:37 +0200

    Add unittests

---
+++ tests/Unit/Operations/Cups/CupsGetPrintersTest.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DR\Ipp\Tests\Unit\Operations\Cups;
+
+use DR\Ipp\Client\IppHttpClientInterface;
+use DR\Ipp\Entity\IppServer;
+use DR\Ipp\Enum\IppOperationEnum;
+use DR\Ipp\Factory\ResponseParserFactoryInterface;
+use DR\Ipp\Operations\Cups\CupsCreatePrinter;
+use DR\Ipp\Operations\Cups\CupsGetPrinters;
+use DR\Ipp\Protocol\IppOperation;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
+use Throwable;
+
+#[CoversClass(CupsCreatePrinter::class)]
+class CupsGetPrintersTest extends TestCase
+{
+    /**
+     * @throws Throwable
+     */
+    public function testCreate(): void
+    {
+        $cups   = 'https://cups';
+        $server = new IppServer();
+        $server->setUri($cups);
+        $server->setUsername('admin');
+        $server->setPassword('admin');
+
+        $client        = $this->createMock(IppHttpClientInterface::class);
+        $parserFactory = $this->createMock(ResponseParserFactoryInterface::class);
+        $parserFactory->expects($this->once())->method('printerResponseParser');
+        $printerCreator   = new CupsGetPrinters($client, $parserFactory);
+        $responseContents = 'test';
+
+        $body = $this->createMock(StreamInterface::class);
+        $body->method('getContents')->willReturn($responseContents);
+        $response = $this->createMock(ResponseInterface::class);
+
+        $client->expects($this->once())->method('sendRequest')->with(static::callback(static function (IppOperation $operation) {
+            static::assertSame(IppOperationEnum::CupsGetPrinters, $operation->getOperation());
+            static::assertCount(0, $operation->getJobAttributes(), 'Job attribute count incorrect');
+            static::assertCount(0, $operation->getPrinterAttributes(), 'Printer attribute count incorrect');
+            static::assertCount(2, $operation->getOperationAttributes(), 'Operation attribute count incorrect');
+
+            return true;
+        }))->willReturn($response);
+
+        $printerCreator->get();
+    }
+}
---
+++ tests/Unit/Protocol/Response/IppGetPrintersResponseParserTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DR\Ipp\Tests\Unit\Protocol\Response;
+
+use DR\Ipp\Entity\IppPrinter;
+use DR\Ipp\Enum\IppOperationEnum;
+use DR\Ipp\Enum\IppStatusCodeEnum;
+use DR\Ipp\Enum\IppTypeEnum;
+use DR\Ipp\Factory\IppPrinterFactory;
+use DR\Ipp\Protocol\IppAttribute;
+use DR\Ipp\Protocol\IppOperation;
+use DR\Ipp\Protocol\Response\IppGetJobsResponseParser;
+use DR\Ipp\Protocol\Response\IppGetPrintersResponseParser;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
+
+#[CoversClass(IppGetJobsResponseParser::class)]
+class IppGetPrintersResponseParserTest extends TestCase
+{
+    public function testGetResponse(): void
+    {
+        $response = new IppOperation(IppOperationEnum::PrintJob);
+        $response->addOperationAttribute(new IppAttribute(IppTypeEnum::Charset, 'attributes-charset', 'utf-8'));
+        $response->addOperationAttribute(new IppAttribute(IppTypeEnum::NaturalLanguage, 'attributes-natural-language', 'en'));
+        $response->addJobAttribute(new IppAttribute(IppTypeEnum::Int, 'printer-name', '1'));
+        $response->addJobAttribute(new IppAttribute(IppTypeEnum::Int, 'printer-name', '2'));
+        $response->addJobAttribute(new IppAttribute(IppTypeEnum::Int, 'printer-name', '3'));
+
+        $printer        = new IppPrinter();
+        $printerFactory = $this->createMock(IppPrinterFactory::class);
+        $printerFactory->method('create')->willReturn($printer);
+        $parser = new IppGetPrintersResponseParser($printerFactory);
+
+        $body = $this->createMock(StreamInterface::class);
+        $body->method('getContents')->willReturn((string)$response);
+        $responseMock = $this->createMock(ResponseInterface::class);
+        $responseMock->method('getBody')->willReturn($body);
+
+        $ippResponse = $parser->getResponse($responseMock);
+        static::assertSame(IppStatusCodeEnum::SuccessfulOkConflictingAttributes, $ippResponse->getStatusCode());
+        static::assertSame([$printer, $printer, $printer], $ippResponse->getPrinters());
+    }
+}
